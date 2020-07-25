bfa1f28a6861f628d19fa58ad37e91c2b786a8c2
MrNukeBlast <bnikolayev993@gmail.com>
2020-07-25 22:53:48 +0200

    Initial commit

---
+++ server/src/main/java/server/DBHandler.java
@@ -0,0 +1,86 @@
+package server;
+
+import java.sql.*;
+
+public class DBHandler {
+    private static Connection connection;
+    private static PreparedStatement psGetNickname;
+    private static PreparedStatement psRegistration;
+
+
+    public static boolean connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+            System.out.println("DataBase connected...");
+            prepareAllStatements();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void prepareAllStatements()  {
+        try {
+            psGetNickname = connection.prepareStatement("SELECT nick FROM userData WHERE login = ? AND password = ?;");
+            psRegistration = connection.prepareStatement("INSERT INTO userData(login, password, nick) VALUES (? ,? ,? );");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+       
+
+
+
+    }
+
+    public static String getNicknameByLoginAndPassword(String login, String password) {
+        String nick = null;
+        try {
+            psGetNickname.setString(1, login);
+            psGetNickname.setString(2, password);
+            ResultSet rs = psGetNickname.executeQuery();
+            if (rs.next()) {
+                nick = rs.getString(1);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return nick;
+    }
+
+    public static boolean registration(String login, String password, String nickname) {
+        try {
+            psRegistration.setString(1, login);
+            psRegistration.setString(2, password);
+            psRegistration.setString(3, nickname);
+            psRegistration.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public static void disconnect() {
+        try {
+            psRegistration.close();
+            psGetNickname.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ server/src/main/java/server/Start.java
@@ -0,0 +1,15 @@
+package server;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class Start {
+    public static void main(String[] args) {
+
+        new Server();
+    }
+
+}
